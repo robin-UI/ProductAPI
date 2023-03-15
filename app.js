f4f6649e975f6c8fb5434807cfc34a9bd9d22595
robin-UI <robinbiju1901@gmail.com>
2023-03-15 16:41:15 +0530

    remove extra stuf

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { notFound, errorHandler } = require('./middleware/errorHandler');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,22 +23,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(notFound)
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(errorHandler)
 
 module.exports = app;
